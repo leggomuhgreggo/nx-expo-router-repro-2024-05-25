f52dc2e345a378ea06ccc491e4232c75e24bf888
Greg Westneat <gregory.westneat@sweetgreen.com>
2024-07-03 23:17:24 -0700

    create new page and try link

--- apps/my-test-app/src/app/dank.tsx
+++ apps/my-test-app/src/app/dank.tsx
@@ -1,13 +1,9 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-import { Link } from 'expo-router';
-
 export default function Page() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home page</Text>
-      <Link href="/">Go to dank page</Link>
-      <Link href="/ddsfsdfdf">Go to dank page</Link>
+      <Text style={styles.text}>DANNNNKKK page</Text>
     </View>
   );
 }
