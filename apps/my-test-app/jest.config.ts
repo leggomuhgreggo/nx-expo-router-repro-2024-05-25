eb32c93617d3a2c7f45b9c1ff35013c6845c9bfe
Greg Westneat <gregory.westneat@sweetgreen.com>
2024-07-03 21:39:56 -0700

    mariecondo

--- apps/my-test-app/jest.config.ts
+++ apps/my-test-app/jest.config.ts
@@ -3,7 +3,6 @@ module.exports = {
   resolver: '@nx/jest/plugins/resolver',
   preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   moduleNameMapper: {
     '\\.svg$': '@nx/expo/plugins/jest/svg-mock',
   },
