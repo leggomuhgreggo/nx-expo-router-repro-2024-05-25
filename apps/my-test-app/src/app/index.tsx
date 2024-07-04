b9aa24fef2055a9604ae299c60affca2b4e650d7
Greg Westneat <gregory.westneat@sweetgreen.com>
2024-07-03 22:08:16 -0700

    💅 class-up the index page

--- apps/my-test-app/src/app/index.tsx
+++ apps/my-test-app/src/app/index.tsx
@@ -1,5 +1,21 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Page() {
-  return <Text>Home page</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home page</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
